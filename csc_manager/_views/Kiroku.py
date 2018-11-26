6cae4cad06b0c621cb678dbaac2e5aec91defcef
Akira Murayama <funya.akira@gmail.com>
2018-11-26 22:07:11 +0900

    2018/11/26 upload

--- csc_manager/_views/Kiroku.py
+++ csc_manager/_views/Kiroku.py
@@ -3,6 +3,7 @@ from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect
 from django.db.models import Q
+from django.conf import settings
 
 from datetime import datetime, date, timedelta
 
@@ -17,6 +18,7 @@ def kiroku_home(request):
     month = date.today().month
     day = date.today().day
     hour = datetime.now().hour
+
     if hour >= 0 and hour < 9:
         day -= 1
 
@@ -40,14 +42,26 @@ class KirokuDayListView(ListView):
         day_night_now = self.kwargs.get('day_night')
 
         target_day = date(year, month, day)
-        kwargs['target_day'] = date(year, month, day)
+        kwargs['target_day'] = target_day
         kwargs['day_night_now'] = day_night_now
 
         if day_night_now == day_night[0][0]:
-            day_night_name = day_night[0][1]
+            kwargs['day_night_name'] = day_night[0][1]
+
+            kwargs['prev_day'] = (target_day - timedelta(days=1))
+            kwargs['prev_day_night'] = day_night[1][0]
+
+            kwargs['next_day'] = target_day
+            kwargs['next_day_night'] = day_night[1][0]
+
         elif day_night_now == day_night[1][0]:
-            day_night_name = day_night[1][1]
-        kwargs['day_night_name'] = day_night_name
+            kwargs['day_night_name'] = day_night[1][1]
+
+            kwargs['prev_day'] = target_day
+            kwargs['prev_day_night'] = day_night[0][0]
+
+            kwargs['next_day'] = (target_day + timedelta(days=1))
+            kwargs['next_day_night'] = day_night[0][0]
 
         kwargs['kirokus'] = Kiroku.objects.filter(exec_date=target_day, day_night=day_night_now).order_by('date', 'time')
 
@@ -59,9 +73,6 @@ class KirokuDayListView(ListView):
         day = self.kwargs.get('day')
         target_day = date(year, month, day)
         yokujitu = target_day + timedelta(days=1)
-        # queryset = Riyosya.objects.filter(
-        #     status=riyosya_status[0][0],
-        # ).order_by('furigana')
 
         if self.kwargs.get('day_night') == day_night[0][0]:
             queryset = Riyosya.objects.filter(
@@ -69,11 +80,11 @@ class KirokuDayListView(ListView):
                 |
                 Q(riyoukikans__start_day__lt=target_day, riyoukikans__last_day__isnull=True)
                 |
-                Q(riyoukikans__start_day=target_day, riyoukikans__start_time__gte='09:00:00', riyoukikans__start_time__lt='18:00:00')
+                Q(riyoukikans__start_day=target_day, riyoukikans__start_time__gte=settings.NIKKIN_START_TIME, riyoukikans__start_time__lt=settings.YAKIN_START_TIME)
                 |
-                Q(riyoukikans__last_day=target_day, riyoukikans__last_time__gte='09:00:00')
+                Q(riyoukikans__last_day=target_day, riyoukikans__last_time__gte=settings.NIKKIN_START_TIME)
                 |
-                Q(riyoukikans__start_day=target_day, riyoukikans__start_time__gte='00:00:00', riyoukikans__start_time__lt='9:00:00', riyoukikans__last_day__isnull=True)
+                Q(riyoukikans__start_day=target_day, riyoukikans__start_time__gte='00:00:00', riyoukikans__start_time__lt=settings.NIKKIN_START_TIME, riyoukikans__last_day__isnull=True)
             ).order_by('furigana')
         elif self.kwargs.get('day_night') == day_night[1][0]:
             queryset = Riyosya.objects.filter(
@@ -81,12 +92,20 @@ class KirokuDayListView(ListView):
                 |
                 Q(riyoukikans__start_day__lte=target_day, riyoukikans__last_day__isnull=True)
                 |
-                Q(riyoukikans__start_day=target_day, riyoukikans__start_time__gte='18:00:00')
+                Q(riyoukikans__start_day=target_day, riyoukikans__start_time__gte=settings.YAKIN_START_TIME)
                 |
-                Q(riyoukikans__start_day=yokujitu, riyoukikans__start_time__lt='09:00:00')
+                Q(riyoukikans__start_day=yokujitu, riyoukikans__start_time__lt=settings.NIKKIN_START_TIME)
                 |
-                Q(riyoukikans__last_day=target_day, riyoukikans__last_time__gte='18:00:00')
+                Q(riyoukikans__last_day=target_day, riyoukikans__last_time__gte=settings.YAKIN_START_TIME)
             ).order_by('furigana')
+
+        # 連続投入用に利用者IDのリストを作ってsessionに格納しておく
+        riyosya_order_list = []
+        for r in queryset:
+            riyosya_order_list.append(r.id)
+
+        self.request.session['riyosya_order_list'] = riyosya_order_list
+
         return queryset
 
 
