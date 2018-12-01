b7646212a19121e5bceead37fc0b8ecc3b1572bb
Akira Murayama <funya.akira@gmail.com>
2018-12-01 19:02:17 +0900

    2018/12/01 upload

--- static/csc_manager/sw.js
+++ static/csc_manager/sw.js
@@ -1,30 +1,28 @@
+var CACHE_DYNAMIC_VERSION = 'dynamic-v1';
 
-// ServiceWorker処理：https://developers.google.com/web/fundamentals/primers/service-workers/?hl=ja
-
-// キャッシュ名とキャッシュファイルの指定
-var CACHE_NAME = 'pwa-sample-caches';
-var urlsToCache = [
-	'/cache/'
-];
-
-// インストール処理
-self.addEventListener('install', function(event) {
-	event.waitUntil(
-		caches
-			.open(CACHE_NAME)
-			.then(function(cache) {
-				return cache.addAll(urlsToCache);
-			})
-	);
-});
-
-// リソースフェッチ時のキャッシュロード処理
 self.addEventListener('fetch', function(event) {
-	event.respondWith(
-		caches
-			.match(event.request)
-			.then(function(response) {
-				return response ? response : fetch(event.request);
-			})
-	);
+  console.log('[Service Worker] Fetching something ...');
+  event.respondWith(
+    // キャッシュの存在チェック
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        } else {
+          // キャッシュがなければリクエストを投げて、レスポンスをキャッシュに入れる
+          return fetch(event.request)
+            .then(function(res) {
+              return caches.open(CACHE_DYNAMIC_VERSION)
+                .then(function(cache) {
+                  // 最後に res を返せるように、ここでは clone() する必要がある
+                  cache.put(event.request.url, res.clone());
+                  return res;
+                })
+            })
+            .catch(function() {
+              // エラーが発生しても何もしない
+            });
+        }
+      })
+  );
 });
